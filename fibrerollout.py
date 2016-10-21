fbc787309676cc540f02095341ef369eceaeb02e
Philip Roche <phil.roche@canonical.com>
2016-10-21 13:01:33 +0100

    Simple GUI created using Gooey
    Binary added for this for Linux too.

--- fibrerollout.py
+++ fibrerollout.py
@@ -5,21 +5,14 @@ python fibrerollout.py --exchange COT
 python fibrerollout.py --exchange COT --exchange KBE
 """
 import argparse
+
 import requests
+from gooey import Gooey
 
 NETWORKMAPS_JS = "http://www.openeir.ie/js/app/eircom.networkmaps.js"
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description='Simple script to pull fibre rollout exchange data')
-    parser.add_argument('--exchange', action="append", dest="exchange_codes",
-                        required=True)
-    args = parser.parse_args()
-
-    if not args.exchange_codes:
-        print("You must specify at least one exchange code.")
-
+def get_exchange_data(exchange_codes):
     networkmaps_js = requests.get(NETWORKMAPS_JS)
     nga_exchange_lists = [line for line in networkmaps_js.text.split('\n') if
                           "com/nga-exchange" in line]
@@ -28,9 +21,9 @@ def main():
         nga_exchange_list = nga_exchange_lists[0]
         nga_exchange_list_url = nga_exchange_list.strip()[7:-2]
         nga_exchange_csv = requests.get(nga_exchange_list_url)
-        for exchange_code in args.exchange_codes:
+        for exchange_code in exchange_codes:
             nga_exchanges = [line for line in nga_exchange_csv.text.split('\n')
-                             if ',%s' % exchange_code in line]
+                             if ',%s' % exchange_code.strip() in line]
             if nga_exchanges:
                 for nga_exchange in nga_exchanges:
                     exchange_data = nga_exchange.strip().split(',')
@@ -38,6 +31,21 @@ def main():
             else:
                 print('No fibre rollout data found for that exchange')
         print('Source: %s' % nga_exchange_list_url)
+
+
+@Gooey
+def main():
+    parser = argparse.ArgumentParser(
+        description='Simple script to pull fibre rollout exchange data')
+    parser.add_argument('--exchange', action="store", dest="exchange_codes",
+                        required=True,
+                        help="Separate multiple exchange codes with comma")
+    args = parser.parse_args()
+
+    if not args.exchange_codes:
+        print("You must specify at least one exchange code.")
+
+    get_exchange_data(args.exchange_codes.split(","))
 
 
 if __name__ == '__main__':
